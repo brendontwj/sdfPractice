84d068af8dfed7ca35a40e4cbf957c90a909358b
brendontwj <bren0019@e.ntu.edu.sg>
2022-06-28 11:22:13 +0800

    init

---
+++ src/main/java/sdfPractice/app/App.java
@@ -0,0 +1,13 @@
+package sdfPractice.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
