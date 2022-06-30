da4d61ec70e983df6b10bf64e33cfc48efeaa010
brendontwj <bren0019@e.ntu.edu.sg>
2022-06-30 16:24:21 +0800

    done

--- src/main/java/sdfPractice/app/HttpClientConnection.java
+++ src/main/java/sdfPractice/app/HttpClientConnection.java
@@ -2,7 +2,6 @@ package sdfPractice.app;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
